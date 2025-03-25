071c34d71d86e4b6d69637d4d1acf33f307639ad
gombasan <xogmlwkdi@naver.com>
2025-03-25 22:58:59 +0900

    Add Spring AOP configuration and logging

--- src/main/java/com/weallys/teams/config/TeamsNotificationAutoConfigure.java
+++ src/main/java/com/weallys/teams/config/TeamsNotificationAutoConfigure.java
@@ -1,11 +1,16 @@
 package com.weallys.teams.config;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.weallys.teams.aop.SectionExtractor;
+import com.weallys.teams.aop.TeamsSpringAop;
 import com.weallys.teams.init.DefaultSectionText;
 import com.weallys.teams.TeamsSectionTextFactory;
 import com.weallys.teams.init.DefaultTeamsNotification;
@@ -14,11 +19,14 @@ import com.weallys.teams.service.TeamsNotification;
 
 @Configuration
 @EnableConfigurationProperties(TeamsProperties.class)
+@ConditionalOnProperty(prefix = "teams.notification", name = "enabled", havingValue = "true")
 public class TeamsNotificationAutoConfigure {
 
 	@Bean
 	@ConditionalOnMissingBean
 	public TeamsNotification teamsNotification(WebClient.Builder builder, TeamsProperties props) {
+		Logger logger = Logger.getLogger(TeamsNotificationAutoConfigure.class.getName());
+		logger.info("[TeamsNotification] Bean is being created with baseUrl : " + props.getBaseUrl());
 		return new DefaultTeamsNotification(builder, props);
 	}
 
@@ -28,4 +36,15 @@ public class TeamsNotificationAutoConfigure {
 		return (DefaultSectionText::new);
 	}
 
+	@Bean
+	@ConditionalOnMissingBean
+	public SectionExtractor sectionExtractor(TeamsSectionTextFactory sectionTextFactory) {
+		return new SectionExtractor(sectionTextFactory);
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public TeamsSpringAop teamsSpringAop(TeamsNotification teamsNotification, SectionExtractor sectionExtractor) {
+		return new TeamsSpringAop(teamsNotification, sectionExtractor);
+	}
 }
